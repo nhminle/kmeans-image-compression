5a2ccc434edb591fd8de566528e8ac0e79639010
Minh Nhat Le <minhle@Minhs-MacBook-Pro.local>
2024-10-06 23:39:14 -0400

    update code

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 img = mpimg.imread('cityview.png')
-img = img.reshape(260, 3, 428, 3, 3)
+height, width = img.shape[0], img.shape[1]
+img = img.reshape(height//3, 3, width//3, 3, 3)
 img = img.transpose(0,2,1,3,4)
-img = img.reshape(260,428, 27)
+img = img.reshape(height//3,width//3, 27)
 img = img.reshape(-1,27)
 
 for k in [2, 5, 10, 50, 200, 500, 1000, 2000]:
@@ -14,12 +15,12 @@ for k in [2, 5, 10, 50, 200, 500, 1000, 2000]:
     kmeans.fit(img)
     centroids  = kmeans.cluster_centers_
     labels = kmeans.labels_
-    labels = labels.reshape(260, 428)
-    newImage = np.zeros((260, 428, 3))
-    for row in range(260):
-        for col in range(428):
-            for channel in range(3):
-                newImage[row, col, channel] = np.mean(centroids[labels[row, col]][channel::3])
+    labels = labels.reshape(height//3, width//3)
+    newImage = np.zeros((height, width, 3))
+    for row in range(height//3):
+        for col in range(width//3):
+            centroid = centroids[labels[row, col]].reshape(3, 3, 3)  # Shape it back to 3x3x3
+            newImage[row*3:(row+1)*3, col*3:(col+1)*3, :] = centroid
     plt.imshow(newImage)
     plt.title(f'Image compression with {k} clusters')
     plt.show()
