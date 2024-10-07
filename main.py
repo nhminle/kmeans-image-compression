a7365c0a6f9e5de40929f8ebd7fbcd26ea80eddd
Minh Nhat Le <minhle@Minhs-MacBook-Pro.local>
2024-10-06 23:11:13 -0400

    update code

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ img = img.transpose(0,2,1,3,4)
 img = img.reshape(260,428, 27)
 img = img.reshape(-1,27)
 
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(img)
-centroids  = kmeans.cluster_centers_
-labels = kmeans.labels_
-labels = labels.reshape(260, 428)
-newImage = np.zeros((260, 428, 3))
-for row in range(260):
-    for col in range(428):
-        for channel in range(3):
-            newImage[row, col, channel] = np.mean(centroids[labels[row, col]][channel::3])
-plt.imshow(newImage)
-plt.show()
+for k in [2, 5, 10, 50, 200, 500, 1000, 2000]:
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(img)
+    centroids  = kmeans.cluster_centers_
+    labels = kmeans.labels_
+    labels = labels.reshape(260, 428)
+    newImage = np.zeros((260, 428, 3))
+    for row in range(260):
+        for col in range(428):
+            for channel in range(3):
+                newImage[row, col, channel] = np.mean(centroids[labels[row, col]][channel::3])
+    plt.imshow(newImage)
+    plt.title(f'Image compression with {k} clusters')
+    plt.show()
